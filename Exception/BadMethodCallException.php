43da963f2509b195b563091b3b0724c2491dde6c
Markus Tacker <m@coderbyheart.de>
2014-11-06 11:49:41 +0100

    Rename exception interface.

--- Exception/BadMethodCallException.php
+++ Exception/BadMethodCallException.php
@@ -1,15 +1,12 @@
 <?php
 
 /**
- * @author    Markus Tacker <m@coderbyheart.de>
- * @copyright 2013 Markus Tacker | coder::by(♥); // Freelance Señor Web Backend Dev | http://coderbyheart.de/
+ * @author    Markus Tacker <m@cto.hiv>
  */
 
 namespace Coderbyheart\MailChimpBundle\Exception;
 
-use Coderbyheart\MailChimpBundle\Exception;
-
-class BadMethodCallException extends \BadMethodCallException implements Exception
+class BadMethodCallException extends \BadMethodCallException implements ExceptionInterface
 {
 
 }
---
+++ Exception/ExceptionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author    Markus Tacker <m@coderbyheart.de>
+ * @copyright 2013-2014 Markus Tacker | @coderbyheart | http://cto.hiv/
+ */
+
+namespace Coderbyheart\MailChimpBundle\Exception;
+
+/**
+ * Package level exception.
+ */
+interface ExceptionInterface
+{
+}
